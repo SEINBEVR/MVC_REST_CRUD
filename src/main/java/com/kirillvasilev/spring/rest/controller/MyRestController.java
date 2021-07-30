9d3c58292ca11fc37b0b0e98de0b536907a31dc8
Kirill Vasilev <kirill.vasilev.work@gmail.com>
2021-07-30 14:27:41 +0300

    #Added update and delete employee functions

--- src/main/java/com/kirillvasilev/spring/rest/controller/MyRestController.java
+++ src/main/java/com/kirillvasilev/spring/rest/controller/MyRestController.java
@@ -32,4 +32,27 @@ public class MyRestController {
         return employee;
     }
 
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmloyee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee==null) {
+            throw new NoSuchEmployeeException("There is no employee with id = " + id + " in database");
+        }
+
+        employeeService.deleteEmployee(id);
+        return "Employee with id =" + id + " has been deleted";
+    }
+
 }
