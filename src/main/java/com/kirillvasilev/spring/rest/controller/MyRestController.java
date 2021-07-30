0cd9fb47dd5991aa84b1c2cde92db872e6946c9d
Kirill Vasilev <kirill.vasilev.work@gmail.com>
2021-07-30 14:08:11 +0300

    #Added global exception handler

--- src/main/java/com/kirillvasilev/spring/rest/controller/MyRestController.java
+++ src/main/java/com/kirillvasilev/spring/rest/controller/MyRestController.java
@@ -1,12 +1,10 @@
 package com.kirillvasilev.spring.rest.controller;
 
 import com.kirillvasilev.spring.rest.entity.Employee;
+import com.kirillvasilev.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.kirillvasilev.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,12 @@ public class MyRestController {
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
+
+        if(employee==null) {
+            throw new NoSuchEmployeeException("There is no employee with id = " + id + "in database");
+        }
+
         return employee;
     }
+
 }
